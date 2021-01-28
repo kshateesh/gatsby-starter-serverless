48d5a25ea34dc6be719bdb8d7970938d658381fc
kshateesh <kshateesh007@gmail.com>
2021-01-28 16:04:34 +0530

    add servers gns

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,18 @@
  */
 
 // You can delete this file if you're not using it
+
+// Move Typography.js styles to the top of the head section so they're loaded first.
+exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+  const headComponents = getHeadComponents()
+  console.log("onPreRenderHTML is Triggered")
+  headComponents.sort((x, y) => {
+    if (x.key === "TypographyStyle") {
+      return -1
+    } else if (y.key === "TypographyStyle") {
+      return 1
+    }
+    return 0
+  })
+  replaceHeadComponents(headComponents)
+}
